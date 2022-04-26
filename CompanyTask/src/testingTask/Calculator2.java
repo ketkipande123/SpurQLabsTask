38954dcdc5d8b2296622c3c7bb15218030ee441f
ketkipande123 <79857710+ketkipande123@users.noreply.github.com>
2022-04-26 23:04:50 +0530

    commit again- added project

--- CompanyTask/src/testingTask/Calculator2.java
+++ CompanyTask/src/testingTask/Calculator2.java
@@ -5,40 +5,46 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class AutoCalculator {
-	
-	static WebDriver driver;
+public class Calculator2 {
 
-	public static void main(String[] args) throws Exception
+//	static WebDriver driver;
+	static WebDriver driver;
+	
+	@Test(priority=1)
+	public void opn() 
 	{
-		System.setProperty("webdriver.chrome.driver", "J:\\Advanto software testing\\Automation\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+//		 System.setProperty("webdriver.chrome.driver", "J:\\Advanto software testing\\Automation\\chromedriver.exe");
+//		  WebDriver driver = new ChromeDriver();
+		  driver.get("https://www.calculator.net/");
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		  driver.get(" https://www.calculator.net/");
-		 
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		//Multiplication
+	}
+	@Test(priority=2)
+	public void multi() throws Exception
+	{
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='4']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='2']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='3']")).click();
 		Thread.sleep(2000);
-		
 		driver.findElement(By.xpath("//div//span[@class='sciop' and text()='×']")).click();
 		Thread.sleep(2000);
-		
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='5']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='2']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='5']")).click();
 		Thread.sleep(2000);
-		
 		driver.findElement(By.xpath("//div//span[@class='scieq' and text()='AC']")).click();
 		Thread.sleep(2000);
-		
-		//division
+
+	}
+	
+	@Test(priority=3)
+	public void div() throws Exception
+	{
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='4']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='0']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='0']")).click();
@@ -55,8 +61,11 @@ public class AutoCalculator {
 		
 		driver.findElement(By.xpath("//div//span[@class='scieq' and text()='AC']")).click();
 		Thread.sleep(2000);
-		
-		//Addition
+	}
+	
+	@Test(priority=4)
+	public void add() throws Exception
+	{
 		driver.findElement(By.xpath("//div//span[@class='sciop' and text()='–']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='2']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='3']")).click();
@@ -79,8 +88,11 @@ public class AutoCalculator {
 		
 		driver.findElement(By.xpath("//div//span[@class='scieq' and text()='AC']")).click();
 		Thread.sleep(2000);
-
-		//Subtraction
+	}
+	
+	@Test(priority=5)
+	public void sub() throws Exception
+	{
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='2']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='3']")).click();
 		driver.findElement(By.xpath("//div//span[@class='scinm' and text()='4']")).click();
@@ -107,8 +119,21 @@ public class AutoCalculator {
 		
 		driver.findElement(By.xpath("//div//span[@class='scieq' and text()='AC']")).click();
 		Thread.sleep(2000);
-		
-		driver.close();
 	}
-
+	  @BeforeClass
+	  public void launchBrowser()
+	  {
+		  System.setProperty("webdriver.chrome.driver", "J:\\Advanto software testing\\Automation\\chromedriver.exe");
+		  driver = new ChromeDriver();
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  }
+	  
+	  @AfterClass
+	  public void close()
+	  {
+		  driver.close();
+	  }
 }
+
+
+
